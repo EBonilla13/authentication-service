e713832d5c4095c2adc79846a58100a6de929ceb
[EBonilla13] <[repoer_13_96@hotmail.com]>
2026-08-18 16:37:54 -0600

    Add the initial project structure and its dependencies

---
+++ auth-service/src/main/java/com/auth/service_authentication/ServiceAuthenticationApplication.java
@@ -0,0 +1,13 @@
+package com.auth.service_authentication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServiceAuthenticationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServiceAuthenticationApplication.class, args);
+	}
+
+}
